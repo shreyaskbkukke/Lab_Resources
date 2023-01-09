838ed8acf0f54e2db82254050f3f904a54f025da
Shreyas KB <shreyaskb333@gmail.com>
2023-01-09 21:20:32 +0530

    Python final code update

--- Python Lab/Part-C/PGRC3.py
+++ Python Lab/Part-C/PGRC3.py
@@ -7,58 +7,66 @@ def insert_data(rno, name, m1, m2,m3):
         record = (rno, name, m1, m2,m3)
         cursor.execute(query, record)
         conn.commit()
-        print("Record inserted successfully into student table")
+        print("\nRecord inserted successfully into student table.")
     except mysql.connector.Error as error:
         print("Failed to insert into MySQL table {}".format(error))
+    print("____________________________________________________________________")
 
 def display():
-    print(".......Details of all students...........")
-    select_Query = "select * from student2"
+    print("\n.......Details of all students...........")
+    select_Query = "select * from student"
     cursor.execute(select_Query)
     
     # get all records
     records = cursor.fetchall()
     print("Total number of rows in table: ", cursor.rowcount)
+    print
     for row in records:
-        print(row[0]," ",row[1]," " ,row[2] ," ",row[3]," ",row[4])
+        print(row[0],"\t",row[1],"\t" ,row[2] ,"\t",row[3],"\t",row[4])
+    print("____________________________________________________________________")
 
 conn = mysql.connector.connect(host='localhost',database='studentdb',user='root',password='')
 cursor = conn.cursor()
 while(1):
-    print("1. To accept the details of students and store it in database")
-    print("2. To display details of students ")
-    print("3. To delete perticular student details ")
-    print("4. exit")
-    c=int(input("Enter Your choice:"))
+    print("\n<<<<~~~~____Menu____~~~~>>>>\n")
+    print("1. To accept the details of students and store it in database.")
+    print("2. To display details of students.")
+    print("3. To delete perticular student details.")
+    print("4. Exit.")
+    c=int(input("Enter Your choice : "))
     if c==1:
-        print("1.To accept the details of students and store it in database")
-        n=int(input("Enter number of students:"))
+        print("____________________________________________________________________")
+        print("\n1.To accept the details of students and store it in database")
+        n=int(input("Enter number of students : "))
         for i in range(n):
-            print ("Enter details of student :",i+1)
-            rno=int(input("Enter Register no:"))
-            name= input("Enter Name:")
-            print("Enter marks in 3 subjects:")
+            print ("\nEnter details of student : ",i+1)
+            rno=int(input("Enter Register no : "))
+            name= input("Enter Name : ") 
+            print("Enter marks in 3 subjects : ")
             m1= int(input())
             m2= int(input())
             m3= int(input())
             insert_data(rno, name, m1, m2,m3)
     elif c==2:
+        print("____________________________________________________________________")
         display()
     elif c==3:
+        print("____________________________________________________________________")
         try:
-            rno=int(input("Enter register no to delete :"))
+            rno=int(input("\nEnter register no to delete :"))
             Delete_query = """Delete from student where rno = %s"""
             
             # row to delete
             cursor.execute(Delete_query, (rno,))
             if cursor.rowcount==0:
-                print("Record not found") 
+                print("\nRecord not found!") 
             else:
                 conn.commit()
-                print("Record Deleted successfully ")
+                print("\nRecord Deleted successfully. ")
                 # display()
         except mysql.connector.Error as error:
-            print("Record not found")
+            print("\nRecord not found!")
+        print("____________________________________________________________________")
     elif c==4:
         break
     else:
--- Python Lab/Part-C/PGRC4.py
+++ Python Lab/Part-C/PGRC4.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-conn = mysql.connector.connect(host='localhost',database='employee',user='root',password='')
+conn = mysql.connector.connect(host='localhost',database='employeedb',user='root',password='')
 cursor = conn.cursor()
 def insert_Edata(eno, name, sal):
     try:
@@ -9,51 +9,62 @@ def insert_Edata(eno, name, sal):
         record = (eno, name, sal)
         cursor.execute(query, record)
         conn.commit()
-        print("Record inserted successfully into employeee table")
+        print("\nRecord inserted successfully into employeee table.")
     except mysql.connector.Error as error:
         print("Failed to insert into MySQL table {}".format(error))
 
 def displayone():
-    print(".......Details of perticular employee...........")
-    eno=int(input("Enter employee No : "))
+    print("...........Details of perticular employee...........")
+    print("To display details of perticular employee.")
+    eno=int(input("\n2Enter employee No : "))
     select_Query = "select * from emp where eno={}".format(eno)
     cursor.execute(select_Query)
     record = cursor.fetchall()
     if(cursor.rowcount==0):
-        print ("Record not found")
+        print ("Record not found!")
     else:
         print(record)
+    print("____________________________________________________________________")
 
 def display():
     print(".......Details of employees based on salary...........")
     min=int(input("Enter Min Salary : "))
     max=int(input("Enter Max Salary : "))
+    print()
+    print('................................')
     q="select * from emp where sal between {} and {}".format(min,max)
     cursor.execute(q)
     records=cursor.fetchall()
     select_Query = "select * from emp where eno=%s"
     for row in records:
-        print(row[0]," ",row[1]," " ,row[2])
+        print(" ",row[0],"\t",row[1],"\t" ,row[2])
+    print('................................')
+    print("\n\n____________________________________________________________________")
 
 while(1):
+    print("\n<<<<~~~~____Menu____~~~~>>>>\n")
     print("1. To accept the details of employees and store it in database")
-    print("2. To display details of perticular employee ")
-    print("3. To display details of employee having salary in a given range")
-    print("4. exit")
-    c=int(input("Enter Your choice:"))
+    print("2. To display details of perticular employee.")
+    print("3. To display details of employee having salary in a given range.")
+    print("4. Exit.")
+    c=int(input("Enter Your choice : "))
     if c==1:
-        n=int(input("Enter number of employees:"))
+        print("____________________________________________________________________")
+        print("\nTo accept the details of employees and store it in database...")
+        n=int(input("Enter number of employees : "))
         for i in range(n):
-            print ("Enter details of employee :",i+1)
-            eno=int(input("Enter employee no:"))
-            name= input("Enter Name:")
-            sal= int(input("Enter salary:"))
+            print ("\nEnter details of employee :",i+1)
+            eno=int(input("Enter employee no : "))
+            name= input("Enter Name : ")
+            sal= int(input("Enter salary : "))
             insert_Edata(eno, name, sal)
+        print("____________________________________________________________________")
     elif c==2 :
-        print("To display details of perticular employee ")
+        print("____________________________________________________________________\n")
         displayone()
     elif c==3:
-        print("To display details of employee having salary in a given range")
+        print("____________________________________________________________________\n")
+        print("To display details of employee having salary in a given range...")
         display()
     elif c==4:
         break
