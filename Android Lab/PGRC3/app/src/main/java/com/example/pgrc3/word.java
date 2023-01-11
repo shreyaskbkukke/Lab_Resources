98e0b120b79213d04f7a081b76f6738f5ff54732
Shreyas KB <shreyaskb333@gmail.com>
2023-01-11 21:25:53 +0530

    Android Update

---
+++ Android Lab/PGRC3/app/src/main/java/com/example/pgrc3/word.java
@@ -0,0 +1,2 @@
+package com.example.pgrc3;public class word {
+}
---
+++ Android Lab/PGRC3/app/src/main/java/com/example/pgrc3/wordAdapter.java
@@ -0,0 +1,14 @@
+package com.example.pgrc3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class wordAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word_adapter);
+    }
+}
